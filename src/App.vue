bc2a43d4ca8075bf31b3f7386d20c98daae0eba7
Darko Goluža <darkogoluza@gmail.com>
2024-09-18 19:03:57 +0200

    implement home page ui

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-	<main>
-		<RouterView />
+	<main class="bg-gray-200 w-ful min-h-screen">
+		<div class="container mx-auto px-4 py-40">
+			<RouterView />
+		</div>
 	</main>
 </template>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,8 +1,64 @@
 <template>
-	<p>Hello World</p>
-	<Button>et</Button>
+	<div class="flex items-end space-x-4">
+		<NumberField :min="1" :max="3" v-model:model-value="val">
+			<Label>Number of players (Bots)</Label>
+			<NumberFieldContent>
+				<NumberFieldDecrement />
+				<NumberFieldInput />
+				<NumberFieldIncrement />
+			</NumberFieldContent>
+		</NumberField>
+	</div>
+
+	<ul class="space-y-2 mt-12">
+		<li v-for="(_, index) in new Array(val)">
+			<div class="flex items-center space-x-4">
+				<p>Bot({{ index + 1 }})</p>
+				<Select v-model:model-value="difficulty[index]">
+					<SelectTrigger class="w-[180px]">
+						<SelectValue />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectGroup>
+							<SelectLabel>Level</SelectLabel>
+							<SelectItem value="easy"> Easy </SelectItem>
+							<SelectItem value="medium"> Medium </SelectItem>
+							<SelectItem value="hard"> Hard </SelectItem>
+						</SelectGroup>
+					</SelectContent>
+				</Select>
+			</div>
+		</li>
+	</ul>
+
+	<Button class="mt-12">
+		<RouterLink to="/game"> Play </RouterLink>
+	</Button>
 </template>
 
 <script setup lang="ts">
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+
+import {
+	NumberField,
+	NumberFieldContent,
+	NumberFieldDecrement,
+	NumberFieldIncrement,
+	NumberFieldInput,
+} from "@/components/ui/number-field";
+import { Label } from "@/components/ui/label";
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
 import { Button } from "@/components/ui/button";
+
+const val = ref(1);
+const difficulty = ref(["medium", "medium", "medium"]);
 </script>
